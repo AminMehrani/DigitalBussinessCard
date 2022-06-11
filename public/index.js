04db7a3f6a9a8f0160bf7bbd1d56c5449e44be0c
unknown <tbag29203@yahoo.com>
2022-06-11 14:39:16 +0430

    v1.1

--- public/index.js
+++ public/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import Joke from "./Joke"
 
 function App() {
     return (
         <div>
         <Navbar />
         <Main />
+        <Joke />
+        <Joke />
+        <Joke />
+        <Joke />
         </div>
     )
 }
---
+++ src/components/Joke.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Joke() {
+    return (
+        <h1>
+            This is a Joke.
+        </h1>
+    )
+}
